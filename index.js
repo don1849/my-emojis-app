e088cb5935b1d2a9adbdd2b140c7267aec0f7e95
don1849 <dek906@sebastiancorp.net>
2022-07-15 21:20:20 -0700

    Update index.js
    Added new emojis to index.js in the 'Second' branch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const myEmojis = ["👨‍💻", "⛷", "🍲", "🤢", "💩", "", "👺"]
 const emojiContainer = document.getElementById('emoji-container')
 let emojiInput = document.getElementById('emoji-input')
 const pushBtn = document.getElementById('push-btn')
